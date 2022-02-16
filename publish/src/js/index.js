423f9d68f82b089faaa6f09f788ead1e20043e3d
ZiuChen <64892985+ZiuChen@users.noreply.github.com>
2022-02-16 23:37:00 +0800

    v1

--- publish/src/js/index.js
+++ publish/src/js/index.js
@@ -1,79 +1,88 @@
-"use strict";
-class Task {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
+let fileObjects = {};
+
+utools.onPluginEnter(({ code, type, payload }) => {
+  $("#config-filename input").val(utools.dbStorage.getItem("config-filename"));
+  $("#config-silence input")[0].checked =
+    utools.dbStorage.getItem("config-silence");
+});
+
+$(".config")
+  .on("click", (e) => {
+    switch (e.currentTarget.id) {
+      case "config-path":
+        let newpath = utools.showOpenDialog({
+          title: "设置路径",
+          defaultPath: utools.getPath("downloads"),
+          buttonLabel: "选择",
+          properties: ["openDirectory", "createDirectory", "promptToCreate"],
+        });
+        if (!newpath) break;
+        utools.dbStorage.setItem("config-path", newpath);
+        break;
+      case "config-restore":
+        restoreConfig();
+        break;
+    }
+  })
+  .on("change", (e) => {
+    switch (e.currentTarget.id) {
+      case "config-filename":
+        utools.dbStorage.setItem("config-filename", e.target.value);
+        break;
+      case "config-silence":
+        utools.dbStorage.setItem("config-silence", e.target.checked);
+        break;
+    }
+  });
+
+$(".transfer-trigger").on("click", (e) => {
+  for (let i = 0; i < $(`tbody > tr`).length; i++) {
+    let item = $(`tbody > tr`)[i];
+    if (
+      item.childNodes[7].innerText.indexOf("上传") === -1 &&
+      item.childNodes[7].innerText.indexOf("转换完毕") === -1 &&
+      item.childNodes[7].innerText.indexOf("下载中") === -1 &&
+      item.childNodes[7].innerText.indexOf("下载完成") === -1
+    ) {
+      // uploaded and file type selected
+      let fileObj = fileObjects[item.id];
+      fileObj.output_type = item.childNodes[7].innerText;
+      executeTransfer(fileObj);
+    }
   }
+  $(".transfer-trigger").hide();
+});
 
-  toString() {
-    return '(' + this.x + ', ' + this.y + ')';
+function init(file) {
+  if (overSize(file)) {
+    toastr.options = { positionClass: "toast-bottom-left" };
+    toastr.error(`${file.name} 大于20MB`, "FileConverter");
+    return;
   }
-}
-
-$(".mdui-tab a").on("click", e => {
-  $(`.pages`).hide()
-  $(`.${e.target.id}`).show()
-})
-
-$(".mdui-fab").on("click", e => {
-  $("#current-file").trigger("click")
-})
-
-$("#current-file").on("change", e => {
-  console.log(e.target.files)
-  init(e.target.files)
-
-})
-
-if(typeof utools !== "undefined") {
-  utools.onPluginEnter(({ code, type, payload }) => {
-    if (type !== "files") return
-    let payld = [
-      {
-        isFile: true,
-        name: "fileName",
-        path: "filePath"
-      }
-    ]
-    init(payload)
+  let fileObj = constructFileObject(file);
+  fileObjects[`${fileObj.id}`] = fileObj;
+  updateTable(fileObj);
+  getExtOutput(fileObj).then((data) => {
+    appendExtInfo(fileObj, data);
+    updateTable(fileObj);
+    $(".transfer-trigger").show();
   });
 }
 
-function init(files) {
-  for(let file of files) {
-    console.log(file);
-    if(checkOverSize(file)) {
-      toastr.options = {
-        positionClass: "toast-bottom-left",
-      }
-      toastr.error(`${file.name} 大于20MB`, "FileConverter")
-    }
-    addNewFile(file)
-  }
-  // let task = new Task(file)
-}
-
-function checkOverSize(file) {
-  console.log(file.size);
-  if(file.size >= 20971520) {
-    console.log("oversize");
-    return true
+function updateTable(fileObj) {
+  if (document.querySelectorAll(`#${fileObj.id}`).length !== 0) {
+    $(`#${fileObj.id} > .file-status`).html(fileObj.status);
   } else {
-    console.log("notoversize");
-    return false
+    const dom = /* html */ `
+        <tr class="file-object" id="${fileObj.id}">
+          <th class="file-name" title="${fileObj.name}">${fileObj.name}</th>
+          <th class="file-type">${fileObj.ext}</th>
+          <th class="file-time">${fileObj.time}</th>
+          <td class="file-status"></td>
+        </tr>
+      `;
+    $(".file-table tbody").prepend(dom);
+    mdui.updateTables();
   }
-}
-
-function addNewFile(file) {
-  let fileID = new Date().getTime()
-  let dom = /* html */`
-    <tr id="${fileID}">
-      <th class="file-name" title="${file.name}">${file.name}</th>
-      <th class="file-type">${file.name.split(".").pop()}</th>
-      <th class="file-process">Process</th>
-      <th class="file-operate">Operate</th>
-    </tr>
-  `
-  $(".file-table tbody").prepend(dom)
-  mdui.updateTables()
+  mdui.mutation();
 }
